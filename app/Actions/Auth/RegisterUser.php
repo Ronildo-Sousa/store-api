541f97482dc3a9b64d932deb2479bd4e03a0751a
Ronildo Sousa <sousaronildo400@gmail.com>
2023-01-13 19:10:25 -0300

    tests for registration

---
+++ app/Actions/Auth/RegisterUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions\Auth;
+
+use App\Actions\Actionable;
+
+class RegisterUser extends Actionable
+{
+    public function handle()
+    {
+        //
+    }
+}
---
+++ tests/Feature/Actions/Auth/RegisterUserTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Models\Admin;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\postJson;
+
+it('should be able to register a customer', function () {
+    postJson(route('api.auth.register', [
+        'name' => fake()->name(),
+        'email' => 'customer@email.com',
+        'password' => 'password'
+    ]))
+        ->assertCreated()
+        ->assertJsonFragment(['token']);
+
+    assertDatabaseHas('users', ['email' => 'customer@email.com']);
+});
+
+test('admin should be able to register a new admin user', function () {
+    $admin = Admin::factory()->create();
+
+    actingAs($admin)
+        ->postJson(route('api.auth.register-admin', [
+            'name' => fake()->name(),
+            'email' => 'newadmin@email.com',
+            'password' => 'password'
+        ]))
+        ->assertCreated();
+
+    assertDatabaseHas('users', ['email' => 'newadmin@email.com']);
+});
+
+test('all fileds should be required and valid', function () {
+    postJson(route('api.auth.register', [
+        'name' => '',
+        'email' => '',
+        'password' => ''
+    ]))
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+    postJson(route('api.auth.register', [
+        'name' => 'test name',
+        'email' => 'some-email',
+        'password' => 'password'
+    ]))
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email' => 'email']);
+});
