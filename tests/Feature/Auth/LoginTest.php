db7a846ef08733f0d6a5412770d33a2ecbd0c564
Ronildo Sousa <sousaronildo400@gmail.com>
2023-01-15 11:19:47 -0300

    tests for login

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Models\Admin;
+use App\Models\Customer;
+
+use function Pest\Laravel\postJson;
+
+beforeEach(function () {
+    $this->customer = Customer::factory()->create();
+    $this->admin = Admin::factory()->create();
+});
+
+it('should be able to login as a customer', function () {
+    postJson(route('api.auth.login', [
+        'email' => $this->customer->email,
+        'password' => 'password'
+    ]))
+        ->assertOk()
+        ->assertJsonStructure(['customer', 'token']);
+});
+
+it('should be able to login as an admin', function () {
+    postJson(route('api.auth.login-admin', [
+        'email' => $this->admin->email,
+        'password' => 'password'
+    ]))
+        ->assertOk()
+        ->assertJsonStructure(['user', 'token']);
+});
+
+test('email should be required and valid', function () {
+    postJson(route('api.auth.login', [
+        'email' => '',
+        'password' => 'password'
+    ]))
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email' => 'required']);
+
+    postJson(route('api.auth.login', [
+        'email' => 'invalid-email',
+        'password' => 'password'
+    ]))
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email' => 'email']);
+});
+
+test('password should be required', function () {
+    postJson(route('api.auth.login', [
+        'email' => 'email@email.com',
+        'password' => ''
+    ]))
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['password' => 'required']);
+});
